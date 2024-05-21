8e5c03b1896eb2f260ac4b1460226378edefdd84
Benjamin Larger <benjaminlarger.bl@gmail.com>
2024-05-21 16:43:37 +0200

    makefile ex02

---
+++ Module00/ex01/Include/phonebook.class.hpp
@@ -0,0 +1,15 @@
+# ifndef PHONEBOOK_CLASS_CPP
+# define PHONEBOOK_CLASS_CPP
+
+class Phonebook
+{
+	public:
+			int		foo;
+			
+			Phonebook(void);
+			~Phonebook(void);
+			
+			void	bar(void);
+};
+
+#endif
