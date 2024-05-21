ba8124ebf5b46943fa1aa95ec0438839dfb4b24f
Benjamin Larger <benjaminlarger.bl@gmail.com>
2024-05-21 15:21:53 +0200

    optimized code

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -3,47 +3,37 @@
 using namespace std;
 
 namespace	megaphone {
-			int	no_noise() {
+			int	no_noise()
+			{
 				std::cout << NO_NOISE << endl;
 				return (0);
 			}
-			void	display_noise(char *noise) {
-				int		i;
+			void	display_noise(char *noise)
+			{
 				bool	has_quotes;
-				i = 0;
-				if (noise[0] == '"')
-				{
-					has_quotes = true;
-					i++;
-				}
-				else
-					has_quotes = false;
+				int		i;
+
+				has_quotes = noise[0] == '"';
+				i = has_quotes ? 1 : 0;
 				while (noise[i] != '\0')
 				{
-					if (noise[i] >= 'a' && noise[i] <= 'z')
-						noise[i] -= 32;
-					std::cout << noise[i];
+					std::cout << static_cast<char>(std::toupper(noise[i]));
 					i++;
-					if (has_quotes == true && noise[i + 1] == '\0')
+					if (has_quotes == true && noise[i] == '"' && noise[i + 1] == '\0')
 						break;
 				}
 			}
 }
 
-using namespace megaphone;
-
 int main(int argc, char **argv)
 {
-    int	i;
-
     if (argc == 1)
-        return (no_noise());
-    i = 1;
-    while (i < argc)
+        return (megaphone::no_noise());
+    for(int i = 1; i < argc; i++)
     {
-		display_noise(argv[i]);
-		i++;
-		std::cout << ' ';
+		megaphone::display_noise(argv[i]);
+		if (i < argc - 1)
+			std::cout << ' ';
     }
 	std::cout << endl;
 }
