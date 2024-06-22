5b3c64241dc34cd9d48534544825fed0047084e2
BenjaminLarger <benjaminlarger.bl@gmail.com>
2024-06-22 12:48:57 +0200

    ex02 Module06 done

--- Module06/ex02/Base.cpp
+++ Module06/ex02/Base.cpp
@@ -26,7 +26,7 @@ C::~C(void){};
 
 Base::~Base(void){};
 
-static Base	*generateA(void)
+/* static Base	*generateA(void)
 {
 	A	a;
 	Base		*basePtr = dynamic_cast<Base*>(&a);
@@ -48,7 +48,7 @@ static Base	*generateC(void)
 	Base		*basePtr = dynamic_cast<Base*>(&c);
 
 	return (basePtr);
-}
+} */
 
 
 Base * Base::generate(void)
@@ -61,27 +61,55 @@ Base * Base::generate(void)
 	switch(randomizedInt)
 	{
 		case 1:
-//			return (generateA());
 			return (new A());
 		case 2:
-//			return (generateB());
 			return (new B());
 
 		case 3:
-//			return (generateC());
 			return (new C());
 	}
 	return (NULL);
 }
 
-void Base::identify(Base* p)
+void Base::identify(Base* base)
 {
-	std::cout << 
-	(void)p;
+
+	A *a = dynamic_cast<A*>(base);
+	if (a)
+		std::cout << "A has been instanced" << std::endl;
+
+    B *b = dynamic_cast<B*>(base);
+	if (b)
+		std::cout << "B has been instanced" << std::endl;
+
+    C *c = dynamic_cast<C*>(base);
+	if (c)
+		std::cout << "C has been instanced" << std::endl;
 }
 
 void Base::identify(Base& p)
 {
-	(void)p;
+	Base	*base = dynamic_cast<Base*>(&p);
+	try
+	{
+        A& a = dynamic_cast<A&>(*base);
+		std::cout << "A has been instanced" << std::endl;
+		(void)a;
+    }
+    catch (std::exception& e) {}
+	try
+	{
+        B& b = dynamic_cast<B&>(*base);
+		std::cout << "B has been instanced" << std::endl;
+		(void)b;
+    }
+    catch (std::exception& e) {}
+	try
+	{
+        C& c = dynamic_cast<C&>(*base);
+		std::cout << "C has been instanced" << std::endl;
+		(void)c;
+    }
+    catch (std::exception& e) {}
 }
 
--- Module06/ex02/main.cpp
+++ Module06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: blarger <blarger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 09:46:47 by blarger           #+#    #+#             */
-/*   Updated: 2024/06/22 11:32:32 by blarger          ###   ########.fr       */
+/*   Updated: 2024/06/22 12:39:23 by blarger          ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -20,6 +20,13 @@
 
 int main()
 {
-    Base::generate();
+	srand(time(NULL));
+
+    Base *ptr = Base::generate();
+	Base::identify(ptr);
+
+	Base	&ref = *ptr; 
+	Base::identify(ref);
+	
     return 0;
 }
